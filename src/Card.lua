f49368498d00999ec581e3f966e57e3a2533cd36
matheo.pons <matheo.pons@epitech.eu>
2024-06-30 14:48:43 +0200

    [UPDATE] : Update for Steamodded 1.0.0 APLHA 0.629b

--- src/Card.lua
+++ src/Card.lua
@@ -30,6 +30,29 @@ function Card:get_ranks(values)
     return ret
 end
 
+function Card:compare_ranks(other_ranks, other_ret)
+    local ret = other_ret or {}
+    local ranks = Card:get_ranks()
+
+    if type(other_ranks) == "table" then
+        for _, rank in ipairs(ranks) do
+            for _, other_rank in ipairs(other_ranks) do
+                if rank == other_rank then
+                    table.insert(ret, other_rank)
+                end
+            end
+        end
+    elseif type(other_ranks) == "number" then
+        for _, rank in ipairs(ranks) do
+            if rank == other_ranks then
+                table.insert(ret, other_ranks)
+            end
+        end
+    end
+
+    return ret
+end
+
 local card_set_debuff_ref = Card.set_debuff
 function  Card:set_debuff(should_debuff)
     if self.ability and self.ability.no_debuff then return end
@@ -44,4 +67,27 @@ end
 local card_is_suit_ref = Card.is_suit
 function Card:is_suit(suit, bypass_debuff, flush_calc)
     return card_is_suit_ref(self, suit, bypass_debuff, flush_calc) or (self.ability and self.ability.all_suits) or false
+end
+
+local calculate_joker_ref = Card.calculate_joker
+function Card:calculate_joker(context)
+    local ret = calculate_joker_ref(self, context) or {}
+
+    if self.ability.set == "Joker" and not self.debuff then
+        if context.individual then
+            if context.cardarea == G.play then
+                if self.ability.name == "Fibonacci" then
+                    if context.other_card:get_id() ~= 2 and context.other_card:get_id() ~= 3 and context.other_card:get_id() ~= 5 and context.other_card:get_id() ~= 8 and context.other_card:get_id() ~= 14 then
+                        if #context.other_card:compare_ranks({2, 3, 5, 8, 14}) > 0 then
+                            ret.mult = ret.mult or 0
+                            ret.card = ret.card or self
+                            ret.mult = ret.mult + self.ability.extra
+                        end
+                    end
+                end
+            end
+        end
+    end
+
+    return ret
 end
